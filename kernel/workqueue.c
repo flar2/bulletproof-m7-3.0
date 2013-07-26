7b2ee5e4e3cd15c7888e4e8e85c32b5d421b58f6
flar2 <asegaert@gmail.com>
2013-07-25 22:49:49 -0400

    linaro fixes part 2

--- kernel/workqueue.c
+++ kernel/workqueue.c
@@ -2519,9 +2519,10 @@ void freeze_workqueues_begin(void)
 		list_for_each_entry(wq, &workqueues, list) {
 			struct cpu_workqueue_struct *cwq;
 			if (cpu < CONFIG_NR_CPUS)
-                                cwq = get_cwq(cpu, wq);
-                        else
-                                continue;
+				cwq = get_cwq(cpu, wq);
+			else
+				continue; 
+
 			if (cwq && wq->flags & WQ_FREEZABLE)
 				cwq->max_active = 0;
 		}
@@ -2546,9 +2547,9 @@ bool freeze_workqueues_busy(void)
 		list_for_each_entry(wq, &workqueues, list) {
 			struct cpu_workqueue_struct *cwq;
 			if (cpu < CONFIG_NR_CPUS)
-                                cwq = get_cwq(cpu, wq);
-                        else
-                                continue;
+				cwq = get_cwq(cpu, wq);
+			else
+				continue; 
 
 			if (!cwq || !(wq->flags & WQ_FREEZABLE))
 				continue;
